37074f59a3b88e00023d18c084b3b09013de7fc2
bmenc <bmenchaca.cca@gmail.com>
2025-07-17 19:21:46 -0600

    feat implement all front pages

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,9 @@
+import UserInfo from "@/components/UserInfo";
+
+export default function Dashboard () {
+    return (
+        <div className="grid place-items-center h-screen">
+            <UserInfo />
+        </div>
+    );
+}
